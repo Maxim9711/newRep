f665ac56cc74335361b7dae4a7c346c078363e4c
Max <Bruce.97@mail.ru>
2023-01-16 15:12:14 +0300

    add task

---
+++ Задание/Program.cs
@@ -0,0 +1,43 @@
+﻿int[] CreateRandomArray(int N, int start, int end){
+    int[] RandomArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandomArray[i] = new Random().Next(start, end + 1);
+    }
+    return RandomArray;
+}
+
+int[] CreateArray(){
+    Console.WriteLine("Введите количество элементов массива");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {   
+        Console.WriteLine($"Введите {i+1} элемент массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void ShowArray(int[] array){
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+
+int [] Array = CreateRandomArray(12, -9, 9);
+ShowArray(Array);
+
+int negative = 0;
+int positive = 0;
+
+for (int i=0; i < Array.Length; i++){
+    if (Array[i] > 0) positive += Array[i];
+    if (Array[i] < 0) negative += Array[i];
+}
+
+Console.WriteLine($"Сумма отрицательных {negative}");
+Console.WriteLine($"Сумма положительных {positive}");
